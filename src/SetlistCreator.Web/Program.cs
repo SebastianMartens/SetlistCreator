c3c328fddfcdb5a487c7179a307119ae8db38c09
Sebastian Martens <sebastian.martens@hotmail.de>
2026-05-16 00:45:29 +0200

    added discogs support

--- src/SetlistCreator.Web/Program.cs
+++ src/SetlistCreator.Web/Program.cs
@@ -8,10 +8,18 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
 builder.Services.AddSetlistServices();
-builder.Services.AddHttpClient<IMusicSearchService, MusicBrainzService>(client =>
+builder.Services.AddHttpClient<MusicBrainzService>(client =>
 {
     client.BaseAddress = new Uri("https://musicbrainz.org/ws/2/");
     client.DefaultRequestHeaders.UserAgent.ParseAdd("SetlistCreator/1.0 (+https://github.com/example/SetlistCreator)");
+});
+var discogsToken = builder.Configuration["Discogs:Token"];
+builder.Services.AddHttpClient<DiscogsService>(client =>
+{
+    client.BaseAddress = new Uri("https://api.discogs.com/");
+    client.DefaultRequestHeaders.UserAgent.ParseAdd("SetlistCreator/1.0 (+https://github.com/example/SetlistCreator)");
+    if (!string.IsNullOrWhiteSpace(discogsToken))
+        client.DefaultRequestHeaders.TryAddWithoutValidation("Authorization", $"Discogs token={discogsToken}");
 });
 
 var app = builder.Build();
